dacce8daad50599162bfefbff1076e8b84ff18dc
Ardiansyah Putra <dwin7609@gmail.com>
2025-10-28 12:26:08 +0700

    Remove unused Component And Adjust Layout Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import FooterHeader from "./footer/FooterHeader";
 
 export default function Footer() {
   return (
-    <footer className="mt-16 bg-emerald-600 text-slate-200">
-  <div className="mx-auto w-full max-w-6xl px-6 py-12 flex justify-center">
+    <footer className="bg-emerald-600 text-slate-200">
+  <div className="mx-auto w-full max-w-6xl px-6 flex justify-center">
     <FooterHeader />
   </div>
 </footer>
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,17 +1,10 @@
-// import HeroSection from "./home/HeroSection";
 import CategoriesSection from "./home/CategoriesSection";
-// import WhyChooseUsSection from "./home/WhyChooseUsSection";
-// import HowItWorksSection from "./home/HowItWorksSection";
-// import ContactSection from "./home/ContactSection";
+
 
 export default function Main() {
   return (
     <div className="w-full space-y-16">
-      {/* <HeroSection /> */}
       <CategoriesSection />
-      {/* <WhyChooseUsSection />
-      <HowItWorksSection />
-      <ContactSection /> */}
     </div>
   );
 }
